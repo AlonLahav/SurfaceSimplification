ca95c3a583bb5a47a03e864a468acb029347a9de
Alon Lahav <alon.lahav2@gmail.com>
2019-06-14 18:45:56 +0300

    Sum bugs fixed and more

--- surface_simplification.py
+++ surface_simplification.py
@@ -2,6 +2,7 @@ import os
 import copy
 import heapq
 
+from tqdm import tqdm
 import numpy as np
 import pylab as plt
 
@@ -9,12 +10,14 @@ import io_off_model
 import mesh_calc
 
 OPTIMAL_POS = False
-CLOSE_DIST_TH = 0.01
+CLOSE_DIST_TH = 0.1
 SAME_V_TH_FOR_PREPROCESS = 0.001
+PRINT_COST = False
 
 def calc_Q_for_each_vertex(mesh):
   mesh_calc.calc_fv_adjacency_matrix(mesh)
   mesh_calc.calc_face_plane_parameters(mesh)
+  mesh['all_v_in_same_plane'] = np.abs(np.diff(mesh['face_plane_parameters'], axis=0)).sum() == 0
   mesh['Qs'] = []
   for v_idx in range(mesh['n_vertices']):
     Q = np.zeros((4, 4))
@@ -25,15 +28,21 @@ def calc_Q_for_each_vertex(mesh):
     mesh['Qs'].append(Q)
 
 def add_pair(mesh, v1, v2, edge_connection):
-  Q = np.sum((mesh['Qs'][v1], mesh['Qs'][v2]))
   new_v1_ = calc_new_vertex_position(mesh, v1, v2)
-  new_v1 = np.vstack((new_v1_[:, None], np.ones((1, 1))))
-  cost = np.dot(np.dot(new_v1.T, Q), new_v1)[0, 0]
+  if mesh['all_v_in_same_plane']:
+    cost = np.linalg.norm(mesh['vertices'][v1] - mesh['vertices'][v2])
+  else:
+    Q = mesh['Qs'][v1] + mesh['Qs'][v2]
+    new_v1 = np.vstack((new_v1_[:, None], np.ones((1, 1))))
+    cost = np.dot(np.dot(new_v1.T, Q), new_v1)[0, 0]
+  if PRINT_COST:
+    print('For pair: ', v1, ',', v2, ' ; the cost is: ', cost)
   heapq.heappush(mesh['pair_heap'], (cost, v1, v2, edge_connection, new_v1_))
 
 def select_vertex_pairs(mesh):
   mesh_calc.calc_v_adjacency_matrix(mesh)
-  for v1 in range(mesh['n_vertices']):
+  print('Calculating pairs cost and add to heap')
+  for v1 in tqdm(range(mesh['n_vertices'])):
     for v2 in range(v1 + 1, mesh['n_vertices']):
       edge_connection = mesh['v_adjacency_matrix'][v1, v2]
       if np.linalg.norm(mesh['vertices'][v2] - mesh['vertices'][v1]) < CLOSE_DIST_TH or edge_connection:
@@ -49,28 +58,34 @@ def calc_new_vertex_position(mesh, v1, v2):
 
 def contract_best_pair(mesh):
   # get pair from heap
-  cost, v1, v2, is_edge, new_v1 = heapq.heappop(mesh['pair_heap'])
+  get_pair = True
+  while get_pair:
+    cost, v1, v2, is_edge, new_v1 = heapq.heappop(mesh['pair_heap'])
+    if v1 not in mesh['forbidden_vertices'] and v2 not in mesh['forbidden_vertices']:
+      get_pair = False
+    if len(mesh['pair_heap']) == 0:
+      return
+  mesh['forbidden_vertices'] += [v1, v2]
 
   # update v1 - position
   mesh['vertices'][v1] = new_v1
 
   # remove v2:
   mesh['vertices'][v2] = [-1, -1, -1] # "remove" vertex from mesh
-  if 0:#is_edge:      # TODO - fix
+  if is_edge:      # TODO - fix
     all_v2_faces = np.where(mesh['fv_adjacency_matrix'][v2])[0]
     for f in all_v2_faces:
-      if v1 in mesh['faces'][f]:
-        mesh['faces'][f] = [-1, -1, -1] # "remove" face from mesh
-      else:
-        v2_idx = np.where(mesh['faces'][f] == v2)[0]
+      if v1 in mesh['faces'][f]:                      # If the face contains v2 also share vertex with v1:
+        mesh['faces'][f] = [-1, -1, -1]               #  "remove" face from mesh.
+      else:                                           # else:
+        v2_idx = np.where(mesh['faces'][f] == v2)[0]  #  replace v2 with v1
         mesh['faces'][f, v2_idx] = v1
   else:
     mesh['faces'][mesh['faces'] == v2] = v1
     idxs = np.where(np.sum(mesh['faces'] == v1, axis=1) > 1)[0]
     mesh['faces'][idxs, :] = -1
-  #mesh['faces']  # - update relevant faces
-  #mesh['egdes'] - connect its edges to v1 ?
-  # remove all v1, v2 pairs from heap
+
+  # remove all v1, v2 pairs from heap (forbidden_vertices can be than removed)
 
   # add new pairs of the new vertex or update the cost of all pairs of v1 (?)
 
@@ -88,13 +103,12 @@ def mesh_preprocess(mesh):
       idxs1 = np.where(mesh['faces'] == v_idx_to_update)
       mesh['faces'][idxs1] = v_idx
 
-def simplify_mesh(mesh_orig, simplification_ratio):
+def simplify_mesh(mesh_orig, n_vertices_to_merge):
   mesh = copy.deepcopy(mesh_orig)
-  return mesh
   mesh_preprocess(mesh)
 
+  mesh['forbidden_vertices'] = []
   mesh['pair_heap'] = []
-  desired_number_of_vertices = mesh_orig['n_vertices'] * simplification_ratio
 
   # Calc Q matrix for eack vertex
   calc_Q_for_each_vertex(mesh)
@@ -102,8 +116,7 @@ def simplify_mesh(mesh_orig, simplification_ratio):
   # Select pairs and add them to a heap
   select_vertex_pairs(mesh)
 
-  #while mesh['n_vertices'] > desired_number_of_vertices:   # TODO ->
-  for _ in range(1):
+  for _ in range(int(n_vertices_to_merge)):
     contract_best_pair(mesh)
 
   clean_mesh_from_removed_items(mesh)
@@ -112,20 +125,37 @@ def simplify_mesh(mesh_orig, simplification_ratio):
 
 def get_mesh(idx=0):
   if idx == -1:
+    mesh = io_off_model.get_simple_mesh('for_mesh_simplification_1')
+    mesh['name'] = 'simple_2d_mesh_1'
+    n_vertices_to_merge = 1
+  elif idx == -2:
     mesh = io_off_model.get_simple_mesh('for_mesh_simplification_2')
     mesh['name'] = 'simple_2d_mesh_2'
+    n_vertices_to_merge = 1
   else:
-    mesh_fns = ['meshes/bottle_0320.off',
-                'meshes/car_0016.off'
+    mesh_fns = [['meshes/bottle_0320.off', 50],
+                ['meshes/person_0067.off', 600],
+                ['meshes/airplane_0359.off', 1000],
+                ['meshes/person_0004.off', 1000],
                 ]
-    mesh = io_off_model.read_off(mesh_fns[idx], verbose=True)
-    mesh['name'] = os.path.split(mesh_fns[idx])[-1]
-  return mesh
+    n_vertices_to_merge = mesh_fns[idx][1]
+    mesh = io_off_model.read_off(mesh_fns[idx][0], verbose=True)
+    mesh['name'] = os.path.split(mesh_fns[idx][0])[-1]
+  return mesh, n_vertices_to_merge
 
-if __name__ == '__main__':
-  mesh = get_mesh(-1)
-  mesh_simplified = simplify_mesh(mesh, 0.5)
+def run_one(mesh_id=4):
+  mesh, n_vertices_to_merge = get_mesh(mesh_id)
+  mesh_simplified = simplify_mesh(mesh, n_vertices_to_merge)
   if not os.path.isdir('output_meshes'):
     os.makedirs('output_meshes')
   fn = 'output_meshes/' + mesh['name'].split('.')[0] + '_simplified.obj'
   io_off_model.write_mesh(fn, mesh_simplified)
+  fn = 'output_meshes/' + mesh['name'].split('.')[0] + '.obj'
+  io_off_model.write_mesh(fn, mesh)
+
+def run_all():
+  for mesh_id in [-2, -1, 0, 1, 2, 3, 4]:
+    run_one(mesh_id)
+
+if __name__ == '__main__':
+  run_all()
