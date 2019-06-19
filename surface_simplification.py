01eabf7866560d3a12abaa51dd4ab77ca2acbeb9
AlonLahav <alon.lahav2@gmai.com>
2019-06-19 11:43:25 +0300

    .

--- surface_simplification.py
+++ surface_simplification.py
@@ -11,11 +11,12 @@ import io_off_model
 import mesh_calc
 
 # Some flags / constants to define the simplification
-ENABLE_NON_EDGE_CONTRACTION = False
+CALC_OPTIMUM_NEW_POINT = False # True / False
+ENABLE_NON_EDGE_CONTRACTION = False # True / False
 CLOSE_DIST_TH = 0.1
 SAME_V_TH_FOR_PREPROCESS = 0.001
 PRINT_COST = False
-SELF_CHECKING = True
+SELF_CHECKING = False # True / False
 np.set_printoptions(linewidth=200)
 
 def calc_Q_for_vertex(mesh, v_idx):
@@ -29,8 +30,6 @@ def calc_Q_for_vertex(mesh, v_idx):
 
 def calc_Q_for_each_vertex(mesh):
   # Prepare some mesh paramenters and run on all vertices to call Q calculation
-  mesh_calc.calc_vf_adjacency_matrix(mesh)
-  mesh_calc.calc_face_plane_parameters(mesh)
   mesh['all_v_in_same_plane'] = np.abs(np.diff(mesh['face_plane_parameters'], axis=0)).sum() == 0
   mesh['Qs'] = []
   for v_idx in range(mesh['n_vertices']):
@@ -51,7 +50,6 @@ def add_pair(mesh, v1, v2, edge_connection):
   heapq.heappush(mesh['pair_heap'], (cost, v1, v2, edge_connection, new_v1_))
 
 def select_vertex_pairs(mesh):
-  mesh_calc.calc_v_adjacency_matrix(mesh)
   print('Calculating pairs cost and add to heap')
   tb = time.time()
   for v1 in tqdm(range(mesh['n_vertices'])):
@@ -72,14 +70,19 @@ def calc_new_vertex_position(mesh, v1, v2, Q):
   # 3. The new vertex will be at the midpoint
   A = Q.copy()
   A[3] = [0, 0, 0, 1]                                 # Defined by eq. (1)
-  A_can_be_ineverted = np.linalg.matrix_rank(A) == 4  # TODO: bug fix!
-  A_can_be_ineverted = False
+  if CALC_OPTIMUM_NEW_POINT:
+    A_can_be_ineverted = np.linalg.matrix_rank(A) == 4  # TODO: bug fix!
+  else:
+    A_can_be_ineverted = False
   if A_can_be_ineverted:
     A_inv = np.linalg.inv(A)
     new_v1 = np.dot(A_inv, np.array([[0, 0, 0, 1]]).T)[:3]
     new_v1 = np.squeeze(new_v1)
   else:
-    new_v1 = look_for_minimum_cost_on_connected_line()
+    if CALC_OPTIMUM_NEW_POINT:
+      new_v1 = look_for_minimum_cost_on_connected_line()
+    else:
+      new_v1 = None
     if new_v1 is None:
       new_v1 = (mesh['vertices'][v1] + mesh['vertices'][v2]) / 2
 
@@ -120,31 +123,7 @@ def contract_best_pair(mesh):
     mesh['faces'][idxs, :] = -1
 
   if SELF_CHECKING:
-    # Check that adjacent matrices coherent to faces list
-    tmp_v_adjacency_matrix = mesh['v_adjacency_matrix'].copy()
-    tmp_vf_adjacency_matrix = mesh['vf_adjacency_matrix'].copy()
-    for f_idx, f in enumerate(mesh['faces']):
-      if f[0] == -1:
-        continue
-      for v1_, v2_ in [(f[0], f[1]), (f[0], f[2]), (f[1], f[2])]:
-        tmp_v_adjacency_matrix[v1_, v2_] = False
-        tmp_v_adjacency_matrix[v2_, v1_] = False
-        if mesh['v_adjacency_matrix'][v1_, v2_] == False or mesh['v_adjacency_matrix'][v2_, v1_] == False:
-          raise Exception('Bad v_adjacency_matrix')
-      for v_ in f:
-        tmp_vf_adjacency_matrix[v_, f_idx] = False
-        if mesh['vf_adjacency_matrix'][v_, f_idx] == False:
-          raise Exception('Bad vf_adjacency_matrix')
-    if np.any(tmp_vf_adjacency_matrix):
-      raise Exception('vf_adjacency_matrix has wrong True elements')
-    #if np.any(tmp_v_adjacency_matrix):
-    #  raise Exception('v_adjacency_matrix has wrong True elements')
-
-    # Check if a face have 2 same vertex indices
-    idxs = np.where(mesh['faces'][:, 0] != -1)[0]
-    to_check = mesh['faces'][idxs]
-    if np.any(np.diff(np.sort(to_check, axis=1), axis=1) == 0):
-      raise Exception('Bug: face found with 2 idintical vertex indices!')
+    check_mesh(mesh)
 
   # remove all v1, v2 pairs from heap (forbidden_vertices can be than removed)
   for pair in mesh['pair_heap'][:]:
@@ -153,7 +132,7 @@ def contract_best_pair(mesh):
 
   # Update Q of vertex v1
   #update_planes_parameters_near_vertex()         --> TODO
-  #calc_Q_for_vertex(mesh, v1)
+  calc_Q_for_vertex(mesh, v1)
 
   # add new pairs of the new vertex
   v2 = None
@@ -162,8 +141,41 @@ def contract_best_pair(mesh):
       continue
     edge_connection = mesh['v_adjacency_matrix'][v1, v2_]
     vertices_are_very_close = ENABLE_NON_EDGE_CONTRACTION and np.linalg.norm(mesh['vertices'][v2] - mesh['vertices'][v1]) < CLOSE_DIST_TH
-    #if edge_connection or vertices_are_very_close:     #  -->> BUG!
-    #  add_pair(mesh, v1, v2_, edge_connection)
+    if edge_connection or vertices_are_very_close:
+      add_pair(mesh, v1, v2_, edge_connection)
+
+def check_mesh(mesh):
+  # Check that there is no duplicated face
+  f_idx_no_erased = np.where(mesh['faces'][:, 1] != -1)[0]
+  real_faces = mesh['faces'][f_idx_no_erased]
+  if np.unique(real_faces, axis=0).shape != real_faces.shape:
+    raise Exception('Duplicated face')
+  # Check that adjacent matrices coherent to faces list
+  tmp_v_adjacency_matrix = mesh['v_adjacency_matrix'].copy()
+  tmp_vf_adjacency_matrix = mesh['vf_adjacency_matrix'].copy()
+  for f_idx, f in enumerate(mesh['faces']):
+    if f[0] == -1:
+      continue
+    for v1_, v2_ in [(f[0], f[1]), (f[0], f[2]), (f[1], f[2])]:
+      tmp_v_adjacency_matrix[v1_, v2_] = False
+      tmp_v_adjacency_matrix[v2_, v1_] = False
+      if mesh['v_adjacency_matrix'][v1_, v2_] == False or mesh['v_adjacency_matrix'][v2_, v1_] == False:
+        raise Exception('Bad v_adjacency_matrix')
+    for v_ in f:
+      tmp_vf_adjacency_matrix[v_, f_idx] = False
+      if mesh['vf_adjacency_matrix'][v_, f_idx] == False:
+        raise Exception('Bad vf_adjacency_matrix')
+  if np.any(tmp_vf_adjacency_matrix):
+    raise Exception('vf_adjacency_matrix has wrong True elements')
+  if np.any(tmp_v_adjacency_matrix):
+    raise Exception('v_adjacency_matrix has wrong True elements')
+
+  # Check if a face have 2 same vertex indices
+  idxs = np.where(mesh['faces'][:, 0] != -1)[0]
+  to_check = mesh['faces'][idxs]
+  if np.any(np.diff(np.sort(to_check, axis=1), axis=1) == 0):
+    raise Exception('Bug: face found with 2 idintical vertex indices!')
+
 
 def clean_mesh_from_removed_items(mesh):
   # Clean up the mesh from faces
@@ -181,6 +193,21 @@ def mesh_preprocess(mesh):
       mesh['vertices'][v_idx_to_update] = [np.nan, np.nan, np.nan]
       idxs1 = np.where(mesh['faces'] == v_idx_to_update)
       mesh['faces'][idxs1] = v_idx
+
+  # Remove suplicated faces
+  for f in mesh['faces']:
+    if f[0] == -1:
+      continue
+    dup_faces = np.where(np.all(mesh['faces'] == f, axis=1))[0][1:]
+    mesh['faces'][dup_faces, :] = -1
+
+  # Prepare mesh
+  mesh_calc.calc_v_adjacency_matrix(mesh)
+  mesh_calc.calc_vf_adjacency_matrix(mesh)
+  mesh_calc.calc_face_plane_parameters(mesh)
+
+  # Make sure the mesh is good now
+  #check_mesh(mesh)
 
 def simplify_mesh(mesh_orig, n_vertices_to_merge):
   mesh = copy.deepcopy(mesh_orig)
@@ -243,4 +270,4 @@ def run_all():
 
 if __name__ == '__main__':
   run_all()
-  #run_one(-1)
+  #run_one(1)
